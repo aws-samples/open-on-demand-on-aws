63359a7a3baf4def24fc4060b8b243cdc9ba8a69
Chris Pollard <polchr@amazon.com>
2022-10-17 20:30:07 -0400

    Remove comment

--- scripts/configure_mkhomedir.sh
+++ scripts/configure_mkhomedir.sh
@@ -7,5 +7,3 @@ systemctl enable oddjobd.service --now
 authconfig --enablesssd --enablesssdauth --enablemkhomedir --updateall
 sed -e '/PasswordAuthentication no/ s/^#*/#/' -i /etc/ssh/sshd_config
 sed -i '/#PasswordAuthentication yes/s/^#//g' /etc/ssh/sshd_config
-
-### Update PubkeyAuthentication to yes
