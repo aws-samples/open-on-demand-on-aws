f1e0638c8d71897eeee50aa9d56c943879b82992
jxuamazon <jianjx@amazon.com>
2023-02-16 08:48:26 -0500

    remove S3FullAccess policy

--- scripts/create_sample_pcluster_config.sh
+++ scripts/create_sample_pcluster_config.sh
@@ -78,7 +78,6 @@ Scheduling:
             - $STACK_NAME
       Iam:
         AdditionalIamPolicies:
-          - Policy: arn:aws:iam::aws:policy/AmazonS3FullAccess
           - Policy: >-
               $COMPUTE_POLICY
     - Name: desktop
@@ -107,7 +106,6 @@ Scheduling:
             - $STACK_NAME
       Iam:
         AdditionalIamPolicies:
-          - Policy: arn:aws:iam::aws:policy/AmazonS3FullAccess
           - Policy: >-
               $COMPUTE_POLICY
   SlurmSettings: {}
