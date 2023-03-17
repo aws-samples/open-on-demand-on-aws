6309741a85ecf192b97355b220ef14051fbde463
Chris Pollard <polchr@amazon.com>
2023-03-17 10:30:27 -0400

    Updates to support interactive desktops

--- scripts/create_sample_pcluster_config.sh
+++ scripts/create_sample_pcluster_config.sh
@@ -4,12 +4,12 @@
 # It needs to read outputs from your OOD stack you already deployed. So you need to have the AWS_PROFILE or access key environment variables set
 # The cluster will have two partitions defined, one for general workload, one for interactive desktop.
 # Please update your
-export STACK_NAME="openondemand-demo"
+export STACK_NAME="ood-demo"
 export SSH_KEY='<your SSH_KEY name>'
 
 
 export REGION="us-east-1"
-export DOMAIN_1="rc"
+export DOMAIN_1="hpclab"
 export DOMAIN_2="local"
 
 export OOD_STACK=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --region $REGION )
@@ -28,8 +28,6 @@ export LDAP_ENDPOINT=$(echo $OOD_STACK | jq -r '.Stacks[].Outputs[] | select(.Ou
 cat << EOF > ../pcluster-config.yml
 HeadNode:
   InstanceType: c5.large
-  Ssh:
-    KeyName: $SSH_KEY
   Networking:
     SubnetId: $SUBNET
     AdditionalSecurityGroups:
