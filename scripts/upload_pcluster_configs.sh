cf3d8dcb5c829dfb311e89d9a7959d7341801af0
Jianjun Xu <jianjx@amazon.com>
2023-09-22 11:36:20 -0400

    add upload v2 scripts

--- scripts/upload_pcluster_configs.sh
+++ scripts/upload_pcluster_configs.sh
@@ -5,3 +5,7 @@
 aws s3 cp pcluster_head_node.sh s3://$CLUSTER_CONFIG_BUCKET/pcluster_head_node.sh
 aws s3 cp pcluster_worker_node.sh s3://$CLUSTER_CONFIG_BUCKET/pcluster_worker_node.sh
 aws s3 cp pcluster_worker_node_desktop.sh s3://$CLUSTER_CONFIG_BUCKET/pcluster_worker_node_desktop.sh
+
+aws s3 cp pcluster_head_node_v2.sh s3://$CLUSTER_CONFIG_BUCKET/pcluster_head_node_v2.sh
+aws s3 cp pcluster_worker_node_v2.sh s3://$CLUSTER_CONFIG_BUCKET/pcluster_worker_node_v2.sh
+aws s3 cp pcluster_worker_node_desktop_v2.sh s3://$CLUSTER_CONFIG_BUCKET/pcluster_worker_node_desktop_v2.sh
