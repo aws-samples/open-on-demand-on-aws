b1c1e55f82b01ed61f0d3135e193d5c8fcd103c5
Jianjun Xu <jianjx@amazon.com>
2023-09-23 23:44:53 -0400

    move the copyright info to after the #! - if not ,the script will fail, causing compute nodes to fail

--- scripts/pcluster_worker_node_desktop.sh
+++ scripts/pcluster_worker_node_desktop.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
+
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
-#!/bin/bash
 
 yum -y -q install jq amazon-efs-utils
 # Get OOD Stack data
--- scripts/upload_pcluster_configs.sh
+++ scripts/upload_pcluster_configs.sh
@@ -1,7 +1,8 @@
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: MIT-0
 #!/bin/bash
 
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
+CLUSTER_CONFIG_BUCKET=<>
 aws s3 cp pcluster_head_node.sh s3://$CLUSTER_CONFIG_BUCKET/pcluster_head_node.sh
 aws s3 cp pcluster_worker_node.sh s3://$CLUSTER_CONFIG_BUCKET/pcluster_worker_node.sh
 aws s3 cp pcluster_worker_node_desktop.sh s3://$CLUSTER_CONFIG_BUCKET/pcluster_worker_node_desktop.sh
