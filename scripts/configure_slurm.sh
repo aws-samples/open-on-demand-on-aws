eb0ec8350bc1357cea3551a7fab2a0c948f12e4c
Doug Morand <domorand@amazon.com>
2025-02-18 13:14:45 -0500

    Adding exclude rules to shellcheck

---
+++ scripts/configure_slurm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
+
+# This script is called whenever a new slurm configuration (slurm.conf, slurmdbd.conf) 
+# is uploaded to the CLUSTER_CONFIG_BUCKET (see config.json). It will update the
+# slurm configuration and restart the slurmctld service.
+
+set -euo pipefail
+
+echo "[-] New slurm configuration(s) detected, restarting services..."
+systemctl restart slurmd
+systemctl restart slurmctld
+systemctl restart slurmdbd
+echo "[-] Finished"
