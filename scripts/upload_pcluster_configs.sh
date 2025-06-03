2456cec129f7e4b4f9f606f5adad080540bc54a0
Doug Morand <domorand@amazon.com>
2025-06-03 16:34:14 -0400

    feat: Upload sackd and configure_slurm scripts to cluster config bucket

--- scripts/upload_pcluster_configs.sh
+++ scripts/upload_pcluster_configs.sh
@@ -2,8 +2,20 @@
 
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
-aws s3 cp pcluster_head_node.sh s3://$CLUSTER_CONFIG_BUCKET/pcluster_head_node.sh
-aws s3 cp pcluster_worker_node.sh s3://$CLUSTER_CONFIG_BUCKET/pcluster_worker_node.sh
-aws s3 cp pcluster_worker_node_desktop.sh s3://$CLUSTER_CONFIG_BUCKET/pcluster_worker_node_desktop.sh
-aws s3 cp configure_login_nodes.sh s3://$CLUSTER_CONFIG_BUCKET/configure_login_nodes.sh
-aws s3 cp configure_pam_slurm_adopt.sh s3://$CLUSTER_CONFIG_BUCKET/configure_pam_slurm_adopt.sh
+# Create an array of scripts to upload
+scripts=(
+    "pcluster_head_node.sh"
+    "pcluster_worker_node.sh"
+    "pcluster_worker_node_desktop.sh"
+    "configure_login_nodes.sh"
+    "configure_pam_slurm_adopt.sh"
+    "configure_slurm_for_ood.sh"
+    "configure_sackd.sh"
+)
+
+# Upload each script to S3
+for script in "${scripts[@]}"; do
+    echo "Uploading $script to s3://$CLUSTER_CONFIG_BUCKET/$script"
+    aws s3 cp "$script" "s3://$CLUSTER_CONFIG_BUCKET/$script"
+done
+
